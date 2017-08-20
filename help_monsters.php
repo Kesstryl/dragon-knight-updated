23e515ddbe754236010e5330b6198707b3ad15c0
Kesstryl <Kesstryl@users.noreply.github.com>
2017-08-19 23:38:17 -0400

    Fixed meta tags

--- help_monsters.php
+++ help_monsters.php
@@ -9,6 +9,7 @@ ob_start("ob_gzhandler");
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <head>
 <title><?php echo $controlrow["gamename"]; ?> Help</title>
 <style type="text/css">
