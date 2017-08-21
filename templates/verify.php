4b3c65b25d9f9a4d9d4191fb685bd14d6d9fcdc7
Kesstryl <Kesstryl@users.noreply.github.com>
2017-08-20 23:49:52 -0400

    Fixed image verification field

--- templates/verify.php
+++ templates/verify.php
@@ -18,6 +18,7 @@ that was emailed to you to unlock your character.</td></tr>
 <tr><td width="20%">Username:</td><td><input type="text" name="username" size="30" maxlength="30" /></td></tr>
 <tr><td>Email Address:</td><td><input type="text" name="email" size="30" maxlength="100" /></td></tr>
 <tr><td>Verification Code:</td><td><input type="text" name="verify" size="10" maxlength="8" /><br /><br /><br /></td></tr>
+<tr><td>Verification:</td><td><img src="auth.php" alt="Image Verification" /><br /><br />Copy the text from the above image into the box below. Can't read it? <a href="users.php?do=register">Refresh</a>.<br /><input id="imagever" name="imagever" type="text" /></td></tr>
 <tr><td colspan="2"><input type="hidden" name="birthday" value=""><input type="submit" name="submit" value="Submit" /> <input type="reset" name="reset" value="Reset" /></td></tr>
 <tr><td><input type="hidden" name="token" value="{$token}" />   </td></tr>
 </table>
